7d2931b070e062b65623d34d2fd5f53774fb13e6
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-03-19 09:32:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include<stdio.h>
-#include<funções.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+#include <pwd.h>
+#include <security/pam_modules.h>
+#include <security/pam_ext.h>
+#include <zconf.h>
 
-void main(){
-    return 0;
-}
+  FILE *arq_recov = fopen("/etc/pam.d/"arquivorecovery".txt", "r");
